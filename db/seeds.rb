5aae7546686463e8ccb7ed8e4828f7a02d025f17
Alain Vallejo <alain.vallejo@laposte.net>
2022-11-21 16:47:59 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+offer.destroy_all
+
+offer = Offer.create!(name: "débroussaillage", category: "jardinage", description: "")
+offer = Offer.create!(name: "tonte", category: "jardinage", description: "")
+offer = Offer.create!(name: "carrelage", category: "bricolage", description: "")
+offer = Offer.create!(name: "peinture", category: "bricolage", description: "")
+offer = Offer.create!(name: "maçonnerie", category: "bricolage", description: "je réalise des murs en parpaing, en briques, en béton")
+offer = Offer.create!(name: "couturier", category: "couture", description: "besoin de modifier, d'adapter ou de retoucher vos vêtements")
+offer = Offer.create!(name: "montage de meuble", category: "bricologe", description: "je vous aide à monter les armoires, les lits et les placards quelque soit la marque ou le modèle !")
+
+puts "terminé"
