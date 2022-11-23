3de24dc6f22d9cf72d2750abde7dc85ed084e7ee
Garry Gabin <gabingarry972@gmail.com>
2022-11-23 11:49:57 -0400

    réalisation du geocoding

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,25 +1,17 @@
 class OffersController < ApplicationController
-  before_action  only: [:show]
-
   def index
     @offers = Offer.all
   end
 
   def show
     # @offer = Offer.new
-    @offers = Offer.all
-   # @markers = @offers.geocoded.map do |offer|
-   #   {
-   #     lat: offer.latitude,
-   #     lng: offer.longitude,
-   #     info_window: render_to_string(partial: "info_window", locals: {offer: offer})
-   #   }
-   # end
+    @offers = Offer.where(id: params[:id])
+    @markers = @offers.geocoded.map do |offer|
+      {
+        lat: offer.latitude,
+        lng: offer.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {offer: offer})
+      }
+    end
   end
-
-  # private
-#
-  # def set_offer
-  #   @offer = Offer.find(params[:id])
-  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,13 @@ user_c = User.create!(first_name: "garry", last_name: "gabin", email: "garry@gma
 user_d = User.create!(first_name: "steph", last_name: "chaigneau", email: "steph@gmail.com", password: "123456")
 
 
-offer_a = Offer.create!(user_id: user_a.id, name: "débroussaillage", category: "jardinage", description: "")
-offer_b = Offer.create!(user_id: user_b.id, name: "tonte", category: "jardinage", description: "")
-offer_c = Offer.create!(user_id: user_c.id, name: "carrelage", category: "bricolage", description: "")
-offer_d = Offer.create!(user_id: user_d.id, name: "peinture", category: "bricolage", description: "")
-offer_e = Offer.create!(user_id: user_d.id, name: "maçonnerie", category: "bricolage", description: "je réalise des murs en parpaing, en briques, en béton")
-offer_f = Offer.create!(user_id: user_d.id, name: "couturier", category: "couture", description: "besoin de modifier, d'adapter ou de retoucher vos vêtements")
-offer_g = Offer.create!(user_id: user_d.id, name: "montage de meuble", category: "bricologe", description: "je vous aide à monter les armoires, les lits et les placards quelque soit la marque ou le modèle !")
+offer_a = Offer.create!(user_id: user_a.id, name: "débroussaillage", category: "jardinage", description: "", address: "Habitation Petit Morne, 97232 Le Lamentin")
+offer_b = Offer.create!(user_id: user_b.id, name: "tonte", category: "jardinage", description: "", address: "Rue de la République, 97200 Fort-de-France")
+offer_c = Offer.create!(user_id: user_c.id, name: "carrelage", category: "bricolage", description: "", address: "Habitation Petit Morne, 97232 Le Lamentin")
+offer_d = Offer.create!(user_id: user_d.id, name: "peinture", category: "bricolage", description: "", address: "Rue de la République, 97200 Fort-de-France")
+offer_e = Offer.create!(user_id: user_d.id, name: "maçonnerie", category: "bricolage", description: "je réalise des murs en parpaing, en briques, en béton", address: "Rue de la République, 97200 Fort-de-France")
+offer_f = Offer.create!(user_id: user_d.id, name: "couturier", category: "couture", description: "besoin de modifier, d'adapter ou de retoucher vos vêtements", address: "Habitation Petit Morne, 97232 Le Lamentin")
+offer_g = Offer.create!(user_id: user_d.id, name: "montage de meuble", category: "bricologe", description: "je vous aide à monter les armoires, les lits et les placards quelque soit la marque ou le modèle !", address: "Rue de la République, 97200 Fort-de-France")
 
 
 booking_a = Booking.create!(user_id: user_a.id, offer_id: offer_a.id, start_time: "2021-11-21", end_time: "2021-11-22")
