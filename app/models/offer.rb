38bf3bda44090f35e9a0ce48364b0977e02d0d24
Kelly Nerestan <jck.martinique@yahoo.com>
2022-11-25 15:45:11 -0400

    okok

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,15 +6,15 @@ class Offer < ApplicationRecord
 
   def image_url
     case category
-    when "jardinage"
+    when "Jardinage"
       "https://www.paysagiste.info/wp-content/uploads/2017/04/se-lancer-en-jardinage-768x0-c-default.jpg"
-    when "bricolage"
+    when "Bricolage"
       "https://www.viepratique.fr/wp-content/uploads/sites/4/2019/04/bricolage-et-diy-quels-outils-avoir-chez-soi.jpg"
-    when "cuisine"
+    when "Cuisine"
       "http://img.over-blog-kiwi.com/0/93/18/18/20160717/ob_634167_colombo0070.JPG"
-    when "informatique"
+    when "Informatique"
       "https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnVsbCUyMHN0YWNrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-    when "sport"
+    when "Sport"
       "https://images.unsplash.com/photo-1556817411-31ae72fa3ea0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNwb3J0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     else
       "https://upload.wikimedia.org/wikipedia/commons/e/e6/Pas_d%27image_disponible.svg"
