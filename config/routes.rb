1d75773994927f26edf3a2bbd8e6efb3ff6ba1fe
Garry Gabin <gabingarry972@gmail.com>
2022-11-28 14:32:53 -0400

    réalisation de la méthode destroy avec son bouton

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
     resources :reviews, only: [:new, :create]
   end
-  resources :bookings, only: [:show, :edit, :update]
+  resources :bookings, only: [:show, :edit, :update, :destroy]
   resources :reviews, only: [:show, :destroy]
 
   resources :bookings, only: :show
