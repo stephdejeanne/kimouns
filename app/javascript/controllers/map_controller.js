0fbd608a57451e637061e9d482af19c34ae32544
Garry Gabin <gabingarry972@gmail.com>
2022-11-29 16:23:56 -0400

    je push sur ta maman

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -21,20 +21,9 @@ export default class extends Controller {
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
 
-      // // Create a HTML element for your custom marker
-      // const customMarker = document.createElement("div")
-      // customMarker.className = "marker"
-      // customMarker.style.backgroundImage = `url('${marker.image_url}')`
-      // customMarker.style.backgroundSize = "contain"
-      // customMarker.style.width = "25px"
-      // customMarker.style.height = "25px"
-
-      // Pass the element as an argument to the new marker
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
         .addTo(this.map)
     })
   }
@@ -43,7 +32,6 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 })
   }
-
 }
 
 // Path: app/javascript/packs/application.js
