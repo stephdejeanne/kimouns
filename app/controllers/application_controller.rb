193eaa4797dbf6ff72c3c4e91ce94e8579de48b7
Garry Gabin <gabingarry972@gmail.com>
2022-11-29 15:56:56 -0400

    réalisation de la navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for
-    root_path
-  end
+  # def after_sign_up_path_for
+  #   root_path
+  # end
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :photo, :phone_number, :skills, :biography])
